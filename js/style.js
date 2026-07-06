1b2d7abeee7c9dcdd0bebd730064a76dfee74f98
johzee <johzee3@gmail.com>
2026-07-06 10:40:49 +0900

    Fix is-hidden bug in diary/flip animations, simplify style.js, fix payment button margin, make ticket cards fully clickable

--- js/style.js
+++ js/style.js
@@ -1,65 +1,59 @@
 $(function () {
 
-  // 퀴즈 14-1: 정답 체크 시 정답 모달 표시
-  (function () {
-    if (!$('.icon_quiz_correct').length) return;
-    var $correct = $('.check_list .checkbox_item').eq(0).find('.checkbox_input');
+  // 퀴즈 14-1/14-2: 체크 시 정답/오답 모달 표시
+  function bindAnswerToggle(iconSelector, checkboxIndex) {
+    if (!$(iconSelector).length) return;
+    var $checkbox = $('.check_list .checkbox_item').eq(checkboxIndex).find('.checkbox_input');
+    var $quizPop = $('.quiz_pop');
+    var $mainQ = $('.main_q');
 
-    $correct.on('change', function () {
+    $checkbox.on('change', function () {
       var isChecked = $(this).is(':checked');
-      $('.quiz_pop').toggleClass('is_hidden', !isChecked);
-      $('.main_q').toggleClass('is_blur', isChecked);
+      $quizPop.toggleClass('is_hidden', !isChecked);
+      $mainQ.toggleClass('is_blur', isChecked);
     });
-  })();
-
-  // 퀴즈 14-2: 오답 체크 시 오답 모달 표시
-  (function () {
-    if (!$('.icon_quiz_wrong').length) return;
-    var $wrong = $('.check_list .checkbox_item').eq(2).find('.checkbox_input');
-
-    $wrong.on('change', function () {
-      var isChecked = $(this).is(':checked');
-      $('.quiz_pop').toggleClass('is-hidden', !isChecked);
-      $('.main_q').toggleClass('is_blur', isChecked);
-    });
-  })();
+  }
+  bindAnswerToggle('.icon_quiz_correct', 0);
+  bindAnswerToggle('.icon_quiz_wrong', 2);
 
   // 퀴즈 16: 드래그 앤 드롭 빈칸 채우기
   (function () {
     var $dragitemsWrap = $('.dragitems_wrap');
     if (!$dragitemsWrap.length) return;
 
-    var $dragging = null;
+    var $dragBox = $('.drag_box');
+    var $quizPop = $('.quiz_pop');
     var $mainQ = $('.main_q');
+    var totalBoxes = $dragBox.length;
+    var $dragging = null;
+
     var $dragboxWrap = $('.dragbox_wrap');
     if ($mainQ.length && $dragboxWrap.length) {
-      var mainQTop = $mainQ[0].getBoundingClientRect().top;
-      var dragboxTop = $dragboxWrap[0].getBoundingClientRect().top;
-      $('.quiz_pop').css('top', (dragboxTop - mainQTop) + 'px');
+      var offsetTop = $dragboxWrap[0].getBoundingClientRect().top - $mainQ[0].getBoundingClientRect().top;
+      $quizPop.css('top', offsetTop + 'px');
     }
 
     $dragitemsWrap.on('dragstart', 'li', function (e) {
-      $dragging = $(this);
-      $(this).addClass('is_dragging');
+      $dragging = $(this).addClass('is_dragging');
       e.originalEvent.dataTransfer.effectAllowed = 'move';
-      e.originalEvent.dataTransfer.setData('text/plain', $(this).text().trim());
+      e.originalEvent.dataTransfer.setData('text/plain', $dragging.text().trim());
     });
 
     $dragitemsWrap.on('dragend', 'li', function () {
       $(this).removeClass('is_dragging');
     });
 
-    $('.drag_box').on('dragover', function (e) {
+    $dragBox.on('dragover', function (e) {
       if ($(this).hasClass('is_filled')) return;
       e.preventDefault();
       $(this).addClass('is_over');
     });
 
-    $('.drag_box').on('dragleave', function () {
+    $dragBox.on('dragleave', function () {
       $(this).removeClass('is_over');
     });
 
-    $('.drag_box').on('drop', function (e) {
+    $dragBox.on('drop', function (e) {
       e.preventDefault();
       $(this).removeClass('is_over');
       if ($(this).hasClass('is_filled') || !$dragging) return;
@@ -72,13 +66,13 @@ $(function () {
         $(this).addClass('is_filled');
         $dragging.removeClass('is_dragging').addClass('is_used');
 
-        if ($('.drag_box').length === $('.drag_box.is_filled').length) {
-          $('.quiz_pop').removeClass('is_hidden');
-          $('.main_q').addClass('is_blur');
+        if ($dragBox.filter('.is_filled').length === totalBoxes) {
+          $quizPop.removeClass('is_hidden');
+          $mainQ.addClass('is_blur');
 
           setTimeout(function () {
-            $('.quiz_pop').addClass('is_hidden');
-            $('.main_q').removeClass('is_blur');
+            $quizPop.addClass('is_hidden');
+            $mainQ.removeClass('is_blur');
           }, 1500);
         }
       } else {
@@ -97,13 +91,13 @@ $(function () {
     if (!$diaryRecord.length) return;
 
     var $payBtn       = $('.split_light .btn_bgDark');
-    var $diaryLis     = $('.diary_record ul li');
-    var $diaryUl      = $('.diary_record ul');
-    var $yodongPocket = $('.diary_record > .pocket:first-child');
-    var $remainPocket = $('.diary_record > .pocket:last-child');
+    var $diaryLis     = $diaryRecord.find('ul li');
+    var $diaryUl      = $diaryRecord.find('ul');
+    var $yodongPocket = $diaryRecord.children('.pocket').first();
+    var $remainPocket = $diaryRecord.children('.pocket').last();
     var $nextBtn      = $('.diray_wrap [class^="btn_line"]');
 
-    // 페이지별로 값이 다르므로(08=1년 전, 09=현재) 마크업에 이미 표시된 값을 그대로 읽어온다
+    // 이미 표시된 값을 그대로 읽어옴
     var products = $diaryLis.map(function () {
       var $li = $(this);
       return {
@@ -116,7 +110,7 @@ $(function () {
     var paidCount = 0, diaryIndex = 0;
 
     // 초기 상태
-    $diaryLis.add($diaryUl).add($remainPocket).add($nextBtn).addClass('is-hidden');
+    $diaryLis.add($diaryUl).add($remainPocket).add($nextBtn).addClass('is_hidden');
     $yodongPocket.find('.body_01, .num_03').text('');
 
     function typewrite($el, text, speed) {
@@ -132,9 +126,14 @@ $(function () {
     }
 
     // 1. 페이지 진입 → 용돈 받음 → 결제하기 활성화
+    function wait(ms) {
+      return new Promise(function (resolve) { setTimeout(resolve, ms); });
+    }
+
     async function startIntro() {
       await typewrite($yodongPocket.find('.body_01'), '용돈 받음');
       await typewrite($yodongPocket.find('.num_03'), totalMoney.toLocaleString());
+      await wait(400);
       $payBtn.prop('disabled', false);
     }
 
@@ -149,13 +148,11 @@ $(function () {
 
       $payBtn.prop('disabled', true);
 
-      for (var idx = 0; idx < toProcess.length; idx++) {
-        var $item = toProcess[idx].$item;
-        var i = toProcess[idx].i;
+      for (const { $item, i } of toProcess) {
         var $li = $diaryLis.eq(diaryIndex);
-        if (diaryIndex === 0) $diaryUl.removeClass('is-hidden');
+        if (diaryIndex === 0) $diaryUl.removeClass('is_hidden');
         $li.find('.body_01, .num_03').text('');
-        $li.removeClass('is-hidden');
+        $li.removeClass('is_hidden');
         await typewrite($li.find('.body_01'), products[i].name);
         await typewrite($li.find('.num_03'), '-' + products[i].price.toLocaleString());
         totalMoney -= products[i].price;
@@ -168,11 +165,11 @@ $(function () {
       // 3. 전체 결제 완료 → 남은 돈
       if (paidCount >= products.length) {
         $remainPocket.find('.body_01, .num_03').text('');
-        $remainPocket.removeClass('is-hidden');
+        $remainPocket.removeClass('is_hidden');
         await typewrite($remainPocket.find('.body_01'), '남은 돈');
         await typewrite($remainPocket.find('.num_03'), totalMoney.toLocaleString());
         // 4. 다음 버튼
-        $nextBtn.removeClass('is-hidden');
+        $nextBtn.removeClass('is_hidden');
       } else {
         $payBtn.prop('disabled', false);
       }
@@ -183,22 +180,23 @@ $(function () {
 
   // 스터디 11: 인플레이션/화폐 착각 카드 플립
   (function () {
-    if (!$('.flip_card').length) return;
+    var $flipCards = $('.flip_card');
+    if (!$flipCards.length) return;
 
     function showComplete() {
-      $('.flip_wrap > .btn_linePrimary2_sm').addClass('is-hidden');
-      $('.btm_btn').removeClass('is-hidden');
+      $('.flip_wrap > .btn_linePrimary2_sm').addClass('is_hidden');
+      $('.btm_btn').removeClass('is_hidden');
     }
 
-    $('.flip_card .card_front .btn_linePrimary').on('click', function () {
+    $flipCards.find('.card_front .btn_linePrimary').on('click', function () {
       $(this).closest('.flip_card').addClass('open');
-      if ($('.flip_card').length === $('.flip_card.open').length) {
+      if ($flipCards.length === $flipCards.filter('.open').length) {
         showComplete();
       }
     });
 
     $('.flip_wrap > .btn_linePrimary2_sm').on('click', function () {
-      $('.flip_card').addClass('open');
+      $flipCards.addClass('open');
       showComplete();
     });
   })();
